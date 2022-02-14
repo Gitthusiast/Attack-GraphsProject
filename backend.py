e2f7d1545eadf296af473c7a31c148bd09209f79
Gitthusiast <dorfmag@epost.bgu.ac.il>
2022-02-14 12:12:40 +0200

    finished working on create xml body tag

--- backend.py
+++ backend.py
@@ -38,16 +38,17 @@ def search_ir_by_head_name(ir_head_name):
     return dp.INTERACTION_RULES_BY_HEAD_NAME.get(ir_head_name)
 
 
-def create_xml(dfMulVAl):
+def create_xml(dfMulVAl, rows):
     """
     :param dfMulVAl: data frame containing the data from the xlsx file
+    :param list of rows to inlcude in the xml
     :return:
     """
     doc = minidom.Document()
     root = doc.createElement('SIRS')
     doc.appendChild(root)
 
-    for row in dp.ROW_TO_IR.keys():
+    for row in rows:
 
         ir_head = dp.ROW_TO_IR[row]
         ir_head_parts = re.split("\(|,|\)", ir_head)
@@ -76,6 +77,31 @@ def create_xml(dfMulVAl):
 
         body = doc.createElement('Body')
         ir.appendChild(body)
+        for interaction_rule in dp.INTERACTION_RULES_BY_HEAD_NAME[ir_head_name]:
+
+            ir_body_parts = re.split("\(|,|\)", interaction_rule[1])
+            head_name = ir_head_parts[0]
+
+            rule = doc.createElement('Rule')
+            rule.setAttribute('Name', head_name)
+
+            if root.firstChild is None:
+                body.appendChild(rule)
+            else:
+                body.insertBefore(rule, body.firstChild)
+            parameters = doc.createElement('Parameters')
+            rule.appendChild(parameters)
+
+            for i, entity in enumerate(ir_body_parts):
+                if i == 0 or entity == '':
+                    continue
+                ent = doc.createElement('Parameter')
+                ent.setAttribute('Type', entity.strip())
+                ent.appendChild(doc.createTextNode(''))
+                if parameters.firstChild is None:
+                    parameters.appendChild(ent)
+                else:
+                    parameters.insertBefore(ent, parameters.firstChild)
 
         desc = doc.createElement('Description')
         explenation = dfMulVAl['Explanation'][row]
@@ -100,4 +126,5 @@ if __name__ == "__main__":
     path = 'C:\\Users\\ADMIN\\Documents\\AttackGraphs\\Attack-GraphsProject\\MulVAL to MITRE-for IR Manager.xlsx'
     dfMulVAl = pd.read_excel(path)
     create_data_structures(dfMulVAl)
-    create_xml(dfMulVAl)
+    rows = dp.ROW_TO_IR.keys()
+    create_xml(dfMulVAl, rows)
