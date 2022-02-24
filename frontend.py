bcbe9b7c215635027de7bd8dafb05c539fb81559
Gitthusiast <dorfmag@epost.bgu.ac.il>
2022-02-24 15:45:17 +0200

    added buttons for further functionality

--- frontend.py
+++ frontend.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
@@ -24,11 +26,15 @@ class grid(MDApp):
         self.search_sir_head = ObjectProperty(None)
         self.search_rule = ObjectProperty(None)
         self.search_in_description = ObjectProperty(None)
+        self.xml_file_path = ObjectProperty(None)
         self.technique_spinner = None
 
     def build(self):
         page_layout = GridLayout(cols=1)
         input_layout = GridLayout(cols=2)
+        buttons_layout = GridLayout(cols=3, spacing=10, padding=[200, 30, 0, 0])
+        import_xml_layout = GridLayout(cols=2)
+        results_layout = BoxLayout(orientation='vertical')
 
         sir_search_label = Label(text='[color=ff000] Search by SIR head name: [/color]', markup=True)
         input_layout.add_widget(sir_search_label)
@@ -92,20 +98,28 @@ class grid(MDApp):
                                                  'Exfiltration Over Physical Medium', 'Derived Environment Rule (NB)',
                                                  'Fact & Derived Environment Rule', 'Disk Wipe'])
         input_layout.add_widget(self.technique_spinner)
-        """
-                Spinner:
-            id: spinner_id
-            text: "technique"
-            size_hint: (0.7, None)
-            height: 35
-            values: ['Derived Environment Rule', 'Fact', 'T1078 - Valid Accounts', 'Exploitation for Client Execution (DT)', 'T1068 - Exploitation for Privilege Escalation (NB)', 'Exploitation for Privilege Escalation (DT)', 'T1190 - Exploit Public-Facing Application (NB)', 'Abuse Elevation Control Mechanism (DT)', 'T1078 - Valid Accounts (NB)', 'T1203 - Exploitation for Client Execution (NB)', 'T1204 - User Execution (NB)', 'T1566 - Phishing (NB)', 'T1005 - Data from Local System (NB)', 'T1003 - OS Credential Dumping (NB)', 'Data from Local System (DT)', 'Building Block (NB)', 'Data from Network Shared Drive (DT)', 'Data from Network Shared Drive', 'Exploitation for Credential Access (DT)', 'T1003- OS Credential Dumping (NB)', 'T1558 - Steal or Forge Kerberos Tickets (NB)', 'TA1078 - Valid Account (NB)', 'Building Block', 'Man-in-the-Middle', 'Endpoint Denial of Service', 'Network Denial of Service', 'Password Policy Discovery', 'Network Sniffing (NB)', 'Network Sniffing', 'T1129 - Shared Modules (NB)', 'T1203 - Exploitation for Client Execution', 'T1204 - User Execution', 'Exploitation for Privilege Escalation', 'T1021 - Remote Services (NB)', 'T1563 - Remote Service Sessions Hijacking (NB)', 'Building Block (DT)', 'T1569 - System Services (NB)', 'T1055 - Process injection', 'T1565 - Data Manipulation', 'External Remote Services (NB)', 'Drive-by Compromise (NB)', 'Remote Service (NB)', 'Steal Application Access Token (NB)', 'Steal or Forge Kerberos Tickets (NB)', 'Command and Scripting Interpreter', 'Exploitation of Remote Services?', 'Remote Services? (DT)', 'Process Injection? (NB)', 'Exfiltration Over Alternative Protocol', 'OS Credential Dumping (NB)', 'Data Manipulation', 'Exploitation for Client Execution', 'Valid Accounts', 'User Execution (NB)', 'Exploit Public-Facing Application (NB)', 'Valid Accounts (DT)', 'Exploitation for Client Execution (NB)', 'Data from Local System', 'Credentials from Password Stores', 'Credentials from Password Stores (DT)', "(Specific to 3d printing, doesn't exist in MITRE yet)", 'Exfiltration Over Other Network Medium', 'File and Directory Discovery (DT)', 'Exfiltration Over Physical Medium', 'Derived Environment Rule (NB)', 'Fact & Derived Environment Rule', 'Disk Wipe']
-            on_text: root.choose_technique(spinner_id.text)
-        """
 
         show_results = \
             Button(text='Show results', size_hint=(None, None), height=35, width=120, pos_hint=(500, 0.7),
                    background_color=(0.3, 0.4, 0.5, 0.7))
         show_results.bind(on_press=self.search)
+
+        export_pddl = Button(text='export pddl', size_hint=(None, None), height=35, width=120, pos_hint=(500, 0.7),
+                             background_color=(0.3, 0.4, 0.5, 0.7))
+
+        export_XML = Button(text='export XML', size_hint=(None, None), height=35, width=120, pos_hint=(500, 0.7),
+                            background_color=(0.3, 0.4, 0.5, 0.7))
+
+        buttons_layout.add_widget(show_results)
+        buttons_layout.add_widget(export_pddl)
+        buttons_layout.add_widget(export_XML)
+
+        self.xml_file_path = TextInput(multiline=True, size_hint=(0.1, None), height=35)
+        import_xml_layout.add_widget(self.xml_file_path)
+
+        import_XML = Button(text='import XML', size_hint=(None, None), height=35, width=120, pos_hint=(500, 0.7),
+                            background_color=(0.3, 0.4, 0.5, 0.7))
+        import_xml_layout.add_widget(import_XML)
 
         self.data_tables = MDDataTable(
             column_data=[
@@ -114,13 +128,15 @@ class grid(MDApp):
                 ("Technique", dp(40))
             ]
         )
+        results_layout.add_widget(self.data_tables)
+
         page_layout.add_widget(input_layout)
-        page_layout.add_widget(show_results)
-        page_layout.add_widget(self.data_tables)
+        page_layout.add_widget(buttons_layout)
+        page_layout.add_widget(import_xml_layout)
+        page_layout.add_widget(results_layout)
         # screen.add_widget(self.data_tables)
         b.build()
         return page_layout
-
 
     def help_search(self, lst, a_list):
         if a_list is not None:
